8ce6f005ab08818c9701adf308188e8fad1dc4a1
Lelton Borges <lelton.borges@fornecedores.sicoob.com.br>
2023-03-17 22:25:15 -0300

    feat:  add args into steps

--- src/main/java/com/example/demo/config/batch/BatchConfig.java
+++ src/main/java/com/example/demo/config/batch/BatchConfig.java
@@ -6,28 +6,39 @@ import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableBatchProcessing
 public class BatchConfig {
+    //    @StepScope
+    @Bean
+    @StepScope
+    public Tasklet getHelloWorld(@Value("#{jobParameters['name']}") String name) {
+        return (StepContribution contribution, ChunkContext chunkContext) -> {
+            System.out.println("Hello World: " + name);
+            return RepeatStatus.FINISHED;
+        };
+    }
 
     @Bean
     public Job job(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory) {
         return jobBuilderFactory.get("imprimeOlaJob")
                                 .start(printHelloStep(stepBuilderFactory))
+                                .incrementer(new RunIdIncrementer())
                                 .build();
     }
 
     @Bean
     public Step printHelloStep(StepBuilderFactory stepBuilderFactory) {
         return stepBuilderFactory.get("imprimeHelloStep")
-                                 .tasklet((StepContribution contribution, ChunkContext chunkContext) -> {
-                                     System.out.println("Hello World");
-                                     return RepeatStatus.FINISHED;
-                                 }).build();
+                                 .tasklet(getHelloWorld(null)).build();
     }
 }
