c88f7c5531242671a859b09cde489c5bdb8e1eca
Lelton Borges <lelton.borges@fornecedores.sicoob.com.br>
2023-03-17 23:02:41 -0300

    feat:  step with chunk

--- src/main/java/com/example/demo/config/batch/BatchConfig.java
+++ src/main/java/com/example/demo/config/batch/BatchConfig.java
@@ -10,10 +10,15 @@ import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.function.FunctionItemProcessor;
+import org.springframework.batch.item.support.IteratorItemReader;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import java.util.List;
+import java.util.stream.IntStream;
 
 @Configuration
 @EnableBatchProcessing
@@ -31,7 +36,8 @@ public class BatchConfig {
     @Bean
     public Job job(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory) {
         return jobBuilderFactory.get("imprimeOlaJob")
-                                .start(printHelloStep(stepBuilderFactory))
+//                                .start(printHelloStep(stepBuilderFactory))
+                                .start(printEvenOdd(stepBuilderFactory))
                                 .incrementer(new RunIdIncrementer())
                                 .build();
     }
@@ -41,4 +47,29 @@ public class BatchConfig {
         return stepBuilderFactory.get("imprimeHelloStep")
                                  .tasklet(getHelloWorld(null)).build();
     }
+
+    public Step printEvenOdd(StepBuilderFactory stepBuilderFactory) {
+        return stepBuilderFactory.get("PrintEvenOdd")
+                                 .<Integer, String>chunk(1)
+                                 .reader(countTenReader())
+                                 .processor(oddOrEvenProcessor())
+                                 .writer(printWriter())
+                                 .build();
+    }
+
+
+    private IteratorItemReader<Integer> countTenReader() {
+        List<Integer> nums = IntStream.rangeClosed(1, 10).boxed().toList();
+        return new IteratorItemReader<>(nums);
+    }
+
+    private FunctionItemProcessor<Integer, String> oddOrEvenProcessor() {
+        return new FunctionItemProcessor<>(i -> i % 2 == 0 ? String.format("Item %s é Par", i) : String.format("Item %s é Impar", i));
+    }
+
+    private ItemWriter<String> printWriter() {
+        return i -> i.forEach(System.out::println);
+    }
+
+
 }
