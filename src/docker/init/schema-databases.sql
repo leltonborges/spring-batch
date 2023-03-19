dce9aa69773ef4617222e9bc4db057b7270d8662
Lelton Borges <lelton.borges@fornecedores.sicoob.com.br>
2023-03-19 20:47:02 -0300

    chore: reorganized application.yaml and fix schema-databases.sql

--- src/docker/init/schema-databases.sql
+++ src/docker/init/schema-databases.sql
@@ -1,7 +1,7 @@
-create schema bratch collate utf8_general_ci;
-create schema app collate utf8_general_ci;
+create schema batch collate utf8_general_ci;
+create schema project collate utf8_general_ci;
 
 create user if not exists project
     identified by 'secret123';
-grant all privileges on bratch.* to project;
-grant all privileges on app.* to project;
+grant all privileges on batch.* to project;
+grant all privileges on project.* to project;
